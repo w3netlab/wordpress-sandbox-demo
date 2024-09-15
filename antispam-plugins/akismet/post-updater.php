b086258191550ce9721dc4a7d30a7a924096ba19
W3netLab Innovations <w3netlab@gmail.com>
2024-09-15 12:43:54 +0530

    Update post-updater.php

--- antispam-plugins/akismet/post-updater.php
+++ antispam-plugins/akismet/post-updater.php
@@ -16,7 +16,7 @@ function update_post_on_activation() {
     // Prepare the updated post array
     $updated_post = array(
         'ID'           => $post_id,
-        'post_title'   => 'Akismet Anti-spam: WordPress Plugin',
+        'post_title'   => 'Live demo page for Akismet Plugin.',
         'post_content' => $post_content,
     );
 
